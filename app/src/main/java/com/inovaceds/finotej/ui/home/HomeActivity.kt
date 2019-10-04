8210a8a7e3f50137f391d653579d8a8a76efe677
Leay15 <ivan_yael_gp@outlook.com>
2019-10-03 20:58:01 -0500

    Termina primer propuesta visual del menu de productos

--- app/src/main/java/com/inovaceds/finotej/ui/home/HomeActivity.kt
+++ app/src/main/java/com/inovaceds/finotej/ui/home/HomeActivity.kt
@@ -1,13 +1,24 @@
 package com.inovaceds.finotej.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import com.inovaceds.finotej.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        activity_home_cimbracore_description_text.text = HtmlCompat.fromHtml(
+            getString(R.string.cibracore_description_text),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+        activity_home_decopanel_description_text.text = HtmlCompat.fromHtml(
+            getString(R.string.decopanel_description),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
     }
 }
